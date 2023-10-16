1f270cf9b316c58d07e42f8f4604c55118434e65
arllyepc <arllyepc@gmail.com>
2023-10-16 13:08:02 -0300

    html pronto

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,65 +1,49 @@
 AOS.init();
 
-/* iniciar swiper js */
+const initSwiper = (mainSelector, thumbSelector, isZoom = false) => {
+  const thumbSwiper = new Swiper(thumbSelector, {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 1,
+    freeMode: true,
+    watchSlidesProgress: true,
+    breakpoints: {
+      320: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+      480: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 6,
+        spaceBetween: 40,
+      },
+    },
+  });
 
-/* var swiper = new Swiper(".swiper-container", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 40,
+  new Swiper(mainSelector, {
+    loop: true,
+    spaceBetween: 10,
+    zoom: isZoom,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
-    1024: {
-      slidesPerView: 1,
-      spaceBetween: 50,
+    thumbs: {
+      swiper: thumbSwiper,
     },
-  },
-});
+  });
+};
 
- */
-
-var swiper = new Swiper(".mySwiper", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 1,
-  freeMode: true,
-  watchSlidesProgress: true,
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 4,
-      spaceBetween: 20,
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 4,
-      spaceBetween: 30,
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 6,
-      spaceBetween: 40,
-    },
-  },
-});
-var swiper2 = new Swiper(".mySwiper2", {
-  zoom: true,
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-});
+// Inicializando os swipers
+initSwiper(".mySwiper2", ".mySwiper", true);
+initSwiper(".mySwiper4", ".mySwiper3", true);
+initSwiper(".mySwiper6", ".mySwiper5", true);
+initSwiper(".mySwiper8", ".mySwiper7", true);
+initSwiper(".mySwiper10", ".mySwiper9", true);
+initSwiper(".mySwiper12", ".mySwiper11", true);
 
 //JQuery: detecta scroll e adiciona a classe scrolled para .header-main suavemente
 
@@ -69,4 +53,23 @@ $(window).scroll(function () {
   } else {
     $(".header-main").removeClass("scrolled");
   }
+});
+
+$(document).ready(function() {
+  // Esconde todos os itens do portfólio
+  $('[data-portfolio-item]').hide();
+  
+  // Mostra o primeiro item do portfólio
+  $('[data-portfolio-item]').first().show();
+  
+  // Faz o clique na lista de portfólio mostrar o respectivo item do portfólio
+  $('.portfolio__item').on('click', function() {
+      var portfolioName = $(this).data('portfolio-nav');
+      $('[data-portfolio-item]').hide();
+      $('[data-portfolio-item="' + portfolioName + '"]').show();
+      
+      // Remove a classe 'active' de todos os itens e adiciona ao item clicado
+      $('.portfolio__item').removeClass('active');
+      $(this).addClass('active');
+  });
 });
